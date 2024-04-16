ed7e52288f6705312babe5039e77ae6ed48444d3
Ratan <ratan.pandey1994@gmail.com>
2024-04-16 09:49:53 +0530

    16 April

--- react/udemy/code/01-starting-project/src/App.jsx
+++ react/udemy/code/01-starting-project/src/App.jsx
@@ -1,39 +1,14 @@
-import reactImg from './assets/react-core-concepts.png';
-import { CORE_CONCEPTS } from './data.js'
 
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are
-        going to build!
-      </p>
-    </header>
-  )
-}
-
-function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
+import { CORE_CONCEPTS } from './data.js';
+import Header from "./assets/components/Header/Header.jsx";
+import CoreConcept from './assets/components/CoreConcept.jsx'
+import TabButton from './assets/components/TabButton.jsx';
 
 function App() {
+
+  function handleSelect() {
+    console.log("Demo");
+  }
   return (
     <div>
       <Header />
@@ -63,7 +38,15 @@ function App() {
             />
           </ul>
         </section>
-        <h2>Time to get started!</h2>
+        <section id='examples'>
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={handleSelect}>Component</TabButton>
+            <TabButton onSelect={handleSelect}>JSX</TabButton>
+            <TabButton onSelect={handleSelect}>Props</TabButton>
+            <TabButton onSelect={handleSelect}>State</TabButton>
+          </menu>
+        </section>
       </main>
     </div>
   );
