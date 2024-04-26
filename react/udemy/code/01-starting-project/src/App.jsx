5923de96b625daf330a6d4739aa316f87edaf7dd
Ratan <ratan.pandey1994@gmail.com>
2024-04-27 00:04:06 +0530

    27 April

--- react/udemy/code/01-starting-project/src/App.jsx
+++ react/udemy/code/01-starting-project/src/App.jsx
@@ -6,8 +6,8 @@ import TabButton from './assets/components/TabButton.jsx';
 
 function App() {
 
-  function handleSelect() {
-    console.log("Demo");
+  function handleSelect(selectedButton) {
+    console.log(selectedButton);
   }
   return (
     <div>
@@ -41,10 +41,10 @@ function App() {
         <section id='examples'>
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={handleSelect}>Component</TabButton>
-            <TabButton onSelect={handleSelect}>JSX</TabButton>
-            <TabButton onSelect={handleSelect}>Props</TabButton>
-            <TabButton onSelect={handleSelect}>State</TabButton>
+            <TabButton onSelect={() => handleSelect('components')}>Component</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
         </section>
       </main>
