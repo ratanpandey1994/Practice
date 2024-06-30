2952e8701385ba5d574e43d6fce87042c15d1e0a
Ratan <ratan.pandey1994@gmail.com>
2024-07-01 00:05:42 +0530

    1 july

---
+++ full stack/javaScript/index.js
@@ -0,0 +1,42 @@
+/* Basic JavaScript */
+
+// let x = 10;
+// let y = 'Ratan';
+// let z = true;
+// console.log(x);
+// console.log(typeof x);
+// console.log(y);
+// console.log(typeof y);
+// console.log(z);
+// console.log(typeof z);
+// console.log(x + " is the value of " + y);
+
+// let name = "Ratan";
+// let surname = "Pandey";
+// console.log(`Hey, My name is ${name} ${surname}`);
+
+/* Type Conversion and String method */
+
+// let a = 56;
+// a = a.toString();
+// console.log(a);
+// console.log(typeof String(a));
+
+/* String method */
+let s = "My name is Ratan";
+// console.log(s[5]);
+// console.log(s.indexOf("n"));
+// console.log(s.lastIndexOf("n"));
+// console.log(s.endsWith("is"));
+// console.log(s.includes("is"));
+// console.log(s.substring(0, 4)); 
+// console.log(s.slice(0, 4));
+// console.log(s.split(" "));
+// console.log(s.replace("is", "was"));
+// console.log(s.length);
+// console.log(s.toLowerCase());
+// console.log(s.toUpperCase());
+// console.log(s.substr(3, 4));
+
+let heading = document.getElementById("myHeading");
+heading.innerText = s;
