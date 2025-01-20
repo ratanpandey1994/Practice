df0543582def2702498cf5c6d46c559eb84f12bb
Ratan <ratan.pandey1994@gmail.com>
2025-01-20 09:18:17 +0530

    20 Jan 2025

--- react/ecommerce/src/Componentns/Header/Navigation/index.js
+++ react/ecommerce/src/Componentns/Header/Navigation/index.js
@@ -10,20 +10,43 @@ const Navigation = () => {
             <div className='container'>
                 <div className='row'>
                     <div className='col-sm-3 navPart1'>
-                        <Button className='allCatTab align-items-center'>
-                            <span className="icon1 me-2"><IoIosMenu /></span>
-                            <span className='text'>All Categories</span>
-                            <span className="icon2 ms-2"><FaAngleDown /></span>
-                        </Button>
+                        <div className="catWrapper">
+                            <Button className='allCatTab align-items-center'>
+                                <span className="icon1 me-2"><IoIosMenu /></span>
+                                <span className='text'>All Categories</span>
+                                <span className="icon2 ms-2"><FaAngleDown /></span>
+                            </Button>
+
+                            <div className="sidebarNav">
+                                <div className='submenu'>
+                                    <ul className="ps-0">
+                                        <li><Link to='/'><Button>Men</Button></Link></li>
+                                        <li><Link to='/'><Button>Women</Button></Link></li>
+                                        <li><Link to='/'><Button>Beauty</Button></Link></li>
+                                        <li><Link to='/'><Button>Watches</Button></Link></li>
+                                        <li><Link to='/'><Button>Kids</Button></Link></li>
+                                        <li><Link to='/'><Button>Gift</Button></Link></li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                     <div className='col-sm-9 navPart2 d-flex align-items-center'>
                         <ul className="list list-inline ms-auto">
                             <li className="list-inline-item"><Link to='/'><Button> Home</Button></Link></li>
-                            <li className="list-inline-item"><Link to='/'><Button>Fashion</Button></Link></li>
-                            <li className="list-inline-item"><Link to='/'><Button>Electronic</Button></Link></li>
-                            <li className="list-inline-item"><Link to='/'><Button>Bakery</Button></Link></li>
-                            <li className="list-inline-item"><Link to='/'><Button>Grocery</Button></Link></li>
-                            <li className="list-inline-item"><Link to='/'><Button>Mobile</Button></Link></li>
+                            <li className="list-inline-item">
+                                <Link to='/'><Button>Men</Button></Link>
+                                <div className='submenu shadow'>
+                                    <Link to='/'><Button>Clothing</Button></Link>
+                                    <Link to='/'><Button>Footwear</Button></Link>
+                                    <Link to='/'><Button>Watches</Button></Link>
+                                </div>
+                            </li>
+                            <li className="list-inline-item"><Link to='/'><Button>Women</Button></Link></li>
+                            <li className="list-inline-item"><Link to='/'><Button>Beauty</Button></Link></li>
+                            <li className="list-inline-item"><Link to='/'><Button>Watches</Button></Link></li>
+                            <li className="list-inline-item"><Link to='/'><Button>Kids</Button></Link></li>
+                            <li className="list-inline-item"><Link to='/'><Button>Gift</Button></Link></li>
                             <li className="list-inline-item"><Link to='/'><Button>Blog</Button></Link></li>
                             <li className="list-inline-item"><Link to='/'><Button>Contact Us</Button></Link></li>
                         </ul>
