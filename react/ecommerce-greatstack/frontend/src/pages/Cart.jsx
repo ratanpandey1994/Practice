24dbfbdb7ce6d45d5f665cd3c3e4797f932c4bcf
Ratan <ratan.pandey1994@gmail.com>
2025-09-05 22:07:56 +0530

    5 September 2025

--- react/ecommerce-greatstack/frontend/src/pages/Cart.jsx
+++ react/ecommerce-greatstack/frontend/src/pages/Cart.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { ShopContext } from '../context/ShopContext'
 
 const Cart = () => {
@@ -6,6 +6,21 @@ const Cart = () => {
     const { products, currency, cartItems } = useContext(ShopContext);
 
     const [cartData, setCartData] = useState([]);
+
+    useEffect(() => {
+        const tempData = [];
+        for (const items in cartItems) {
+            for (const item in cartItems[items]) {
+                if (cartItems[items][item] > 0) {
+                    tempData.push({
+                        _id: items,
+                        size: item,
+                        quantity: cartItems[items][item],
+                    })
+                }
+            }
+        }
+    }, [cartItems])
 
     return (
         <div>Cart</div>
